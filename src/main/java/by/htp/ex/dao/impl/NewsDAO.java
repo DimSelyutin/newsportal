e70d07e585d8650d5d50836c5eacbbb062dc6d7e
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2023-01-05 22:18:59 +0300

    comit

--- src/main/java/by/htp/ex/dao/impl/NewsDAO.java
+++ src/main/java/by/htp/ex/dao/impl/NewsDAO.java
@@ -16,19 +16,19 @@ import by.htp.ex.dao.connectionPool.ConnectionPoolException;
 import java.sql.ResultSet;
 
 public class NewsDAO implements INewsDAO {
-    private Connection con =  DaoProvider.getInstance().getConnectionDAO().getConnection();
+    private Connection con;
     private ResultSet rs;
 
     @Override
     public void addNews(News news) throws SQLException {
-        
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         String sqlQuery = String.format("INSERT INTO `posts` (`title`, `text`, `image`, `date_post`, `user_id`, `idCategory`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')",news.getTitle(),news.getText(), news.getImageDir(), news.getPostDate(), news.getUserId(), news.getCategory());
         con.prepareStatement(sqlQuery).executeUpdate();
     }
 
     @Override
     public void update(News news) throws SQLException, ConnectionPoolException {
-        
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         news.setPostDate(news.onCreate());
         String sqlAllNews = "UPDATE `posts` SET `title` = '"+ news.getTitle() + "', `text` = '"+ news.getText()+ "', `image` = '"+ news.getImageDir()+ "', `date_post` = '" 
         + news.getPostDate() + "', `user_id` = '" 
@@ -48,7 +48,7 @@ public class NewsDAO implements INewsDAO {
     @Override
     public List<Category> findAllCategory() throws SQLException {
         List<Category> categoryList = new ArrayList<>();
-        
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         String sqlCategory = "SELECT * FROM category";
         
         rs = con.createStatement().executeQuery(sqlCategory);
@@ -62,7 +62,7 @@ public class NewsDAO implements INewsDAO {
     @Override
     public News getNews(String idPost) throws ConnectionPoolException, SQLException, DaoException {
         rs = null;
-       
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         String sqlAllNews = "SELECT * FROM posts where `id`='" + idPost + "'";
         String strCategory = getCategoryForNews(idPost);
 
@@ -77,9 +77,9 @@ public class NewsDAO implements INewsDAO {
 
     public List<News> getAllNews() throws ConnectionPoolException, SQLException, DaoException {
         List<Category> listCategory = findAllCategory();
-        
+        con = null;
         rs = null;
-        
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         List<News> listok = new ArrayList<>();
         String sqlAllNews = "SELECT * FROM posts";
         rs = con.createStatement().executeQuery(sqlAllNews);
@@ -101,7 +101,7 @@ public class NewsDAO implements INewsDAO {
     public String getCategoryForNews(String idCategory) throws SQLException, ConnectionPoolException, DaoException {
         String category = null;
         String sqlPostCategory = "SELECT * FROM category where `id`='" + idCategory + "'";
-       
+        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
         rs = con.createStatement().executeQuery(sqlPostCategory);
 
         if (rs.next()) {
