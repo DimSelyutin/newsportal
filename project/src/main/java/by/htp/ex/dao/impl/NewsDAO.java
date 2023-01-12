76332edd31e3d948fd9ff07372d0bbdc37188792
Selyutin Dmitriy <dima.selyutn.9@mail.ru>
2023-01-12 16:38:35 +0300

    Change chain of exceptions

--- project/src/main/java/by/htp/ex/dao/impl/NewsDAO.java
+++ project/src/main/java/by/htp/ex/dao/impl/NewsDAO.java
@@ -10,115 +10,153 @@ import by.htp.ex.bean.News;
 import by.htp.ex.dao.DaoException;
 import by.htp.ex.dao.DaoProvider;
 import by.htp.ex.dao.INewsDAO;
-import by.htp.ex.dao.connectionPool.ConnectionPoolException;
+
 
 import java.sql.ResultSet;
 
 public class NewsDAO implements INewsDAO {
     private Connection con;
     private ResultSet rs;
+    private final String MSG_ERR = "Some problems whith database";
+    private final String INSERT_NEWS = "INSERT INTO `posts` (`title`, `text`, `image`, `date_post`, `user_id`, `idCategory`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')";
+    private final String UPDATE_NEWS = "UPDATE `posts` SET `title` = '%s', `text` = '%s', `image` = '%s', `date_post` = '%s', `user_id` = '%s' WHERE (`id` = '%s')";
+    private final String DELETE_NEWS = "DELETE FROM `posts` WHERE (`id` = '%s')";
+    private final String SELECT_CATEGORY = "SELECT * FROM category";
+    private final String SELECT_POST_ID = "SELECT * FROM posts where `id`= '%s'";
+    private final String S_POSTS_CATEGORY = "SELECT * FROM posts, category where `idCategory`= category.id";
+    private final String S_POSTS_CATEGORY_ID = "SELECT * FROM posts, category where posts.idCategory=category.id and posts.id = '%s'";
+    private final String S_POSTS_CATEGPRY_CNAME = "SELECT * FROM posts, category where `idCategory`= category.id and category_name ='%s'";
 
     @Override
-    public void addNews(News news) throws SQLException {
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        String sqlQuery = String.format("INSERT INTO `posts` (`title`, `text`, `image`, `date_post`, `user_id`, `idCategory`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')",news.getTitle(),news.getText(), news.getImageDir(), news.getPostDate(), news.getUserId(), news.getCategory());
-        con.prepareStatement(sqlQuery).executeUpdate();
-    }
-
-    @Override
-    public void update(News news) throws SQLException, ConnectionPoolException {
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        news.setPostDate(news.onCreate());
-        String sqlAllNews = "UPDATE `posts` SET `title` = '"+ news.getTitle() + "', `text` = '"+ news.getText()+ "', `image` = '"+ news.getImageDir()+ "', `date_post` = '" 
-        + news.getPostDate() + "', `user_id` = '" 
-        + news.getUserId()+ "' WHERE (`id` = '" 
-        + news.getId() + "')";
-        PreparedStatement prepStatement = con.prepareStatement(sqlAllNews);
-        prepStatement.executeUpdate();
-
-    }
-
-    @Override
-    public void deleteNews(String idNews) throws SQLException {
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        String sqlDel = String.format("DELETE FROM `posts` WHERE (`id` = '%s')",idNews);
-        con.prepareStatement(sqlDel).executeUpdate(sqlDel);
-    }
-
-    @Override
-    public List<Category> findAllCategory() throws SQLException {
-        List<Category> categoryList = new ArrayList<>();
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        String sqlCategory = "SELECT * FROM category";
-        
-        rs = con.createStatement().executeQuery(sqlCategory);
-        while(rs.next()){
-            categoryList.add(new Category(rs.getInt(1),rs.getString(2), rs.getInt(3)));
+    public void addNews(News news) throws DaoException {
+        try {
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            String sqlQuery = String.format(
+                    INSERT_NEWS,
+                    news.getTitle(), news.getText(), news.getImageDir(), news.getPostDate(), news.getUserId(),
+                    news.getCategory());
+            con.prepareStatement(sqlQuery).executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
         }
-        rs = null;
-        return categoryList;
     }
 
     @Override
-    public News getNews(String idPost) throws ConnectionPoolException, SQLException, DaoException {
-        rs = null;
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        String sqlAllNews = String.format("SELECT * FROM posts, category where posts.idCategory=category.id and posts.id = '%s'",idPost);
-        
+    public void update(News news) throws DaoException {
+        try {
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            news.setPostDate(news.onCreate());
+            String sqlAllNews = String.format(UPDATE_NEWS, news.getTitle(),news.getText(),news.getImageDir(),news.getPostDate(), news.getUserId(),news.getId());
+            PreparedStatement prepStatement = con.prepareStatement(sqlAllNews);
+            prepStatement.executeUpdate();
 
-        rs = con.createStatement().executeQuery(sqlAllNews);
-        News news = null;
-        while (rs.next()) {
-            news = new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
-                    rs.getString(9), rs.getInt(6));
+        } catch (SQLException e) {
+            // TODO: handle exception
         }
-        return news;
     }
 
+    @Override
+    public void deleteNews(String idNews) throws DaoException {
+        try {
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            String sqlDel = String.format(DELETE_NEWS, idNews);
+            con.prepareStatement(sqlDel).executeUpdate(sqlDel);
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
+        }
+    }
 
-    public List<News> findByCategory(String category)  throws ConnectionPoolException, SQLException, DaoException {
+    @Override
+    public List<Category> findAllCategory() throws DaoException {
+        try {
+            List<Category> categoryList = new ArrayList<>();
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            rs = con.createStatement().executeQuery(SELECT_CATEGORY);
+            while (rs.next()) {
+                categoryList.add(new Category(rs.getInt(1), rs.getString(2), rs.getInt(3)));
+            }
+            rs = null;
+            return categoryList;
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
+
+        }
+    }
+
+    @Override
+    public News getNews(String idPost) throws DaoException {
+        try {
+            rs = null;
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            String sqlAllNews = String.format(S_POSTS_CATEGORY_ID, idPost);
+
+            rs = con.createStatement().executeQuery(sqlAllNews);
+            News news = null;
+            while (rs.next()) {
+                news = new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
+                        rs.getString(9), rs.getInt(6));
+            }
+            return news;
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
+
+        }
+
+    }
+
+    public List<News> findByCategory(String category) throws DaoException {
         con = null;
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        
-        List<News> listok2 = new ArrayList<>();
-        String sqlByCategory = String.format("SELECT * FROM posts, category where `idCategory`= category.id and category_name ='%s';", category);
-        rs = con.createStatement().executeQuery(sqlByCategory);
-        while (rs.next()) {
-            listok2.add(new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
-            rs.getString(9), rs.getInt(6)));
+        try {
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+
+            List<News> listok2 = new ArrayList<>();
+            String sqlByCategory = String.format(
+                S_POSTS_CATEGPRY_CNAME, category);
+            rs = con.createStatement().executeQuery(sqlByCategory);
+            while (rs.next()) {
+                listok2.add(new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
+                        rs.getString(9), rs.getInt(6)));
+            }
+            return listok2;
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
         }
-        return listok2;
     }
 
-
-    public List<News> getAllNews() throws ConnectionPoolException, SQLException, DaoException {
-        con = null;
-        rs = null;
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        List<News> listok = new ArrayList<>();
-        String sqlAllNews = "SELECT * FROM posts, category where `idCategory`= category.id;";
-        rs = con.createStatement().executeQuery(sqlAllNews);
-        while (rs.next()) {
-            listok.add(new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
-            rs.getString(9), rs.getInt(6)));
+    public List<News> getAllNews() throws DaoException {
+        try {
+            con = null;
+            rs = null;
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            List<News> listok = new ArrayList<>();
+           
+            rs = con.createStatement().executeQuery(S_POSTS_CATEGORY);
+            while (rs.next()) {
+                listok.add(new News(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(5), rs.getString(4),
+                        rs.getString(9), rs.getInt(6)));
+            }
+            return listok;
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
         }
-        return listok;
     }
 
     @Override
-    public void writeLike(String idNews) throws SQLException {
-        con = DaoProvider.getInstance().getConnectionDAO().getConnection();
-        String sqlgetLike = "SELECT * FROM posts where `id`= '"+idNews+"';";
-        rs = con.createStatement().executeQuery(sqlgetLike);
-        int likeCount=0;
-        if (rs.next()) {
-            likeCount = rs.getInt(8);
+    public void writeLike(String idNews) throws DaoException {
+        try {
+            con = DaoProvider.getInstance().getConnectionDAO().getConnection();
+            String sqlgetLike = String.format(SELECT_POST_ID, idNews);
+            rs = con.createStatement().executeQuery(sqlgetLike);
+            int likeCount = 0;
+            if (rs.next()) {
+                likeCount = rs.getInt(8);
+            }
+            String sqlLike = String.format("UPDATE `posts` SET `likeCount` = '%s' WHERE (`id` = '%s');",
+                    ++likeCount, idNews);
+        } catch (SQLException e) {
+            throw new DaoException(MSG_ERR, e);
         }
-        String sqlLike = String.format("UPDATE `vibestretch`.`posts` SET `likeCount` = '%s' WHERE (`id` = '%s');",++likeCount,idNews);
-        
-    }
 
-  
-    
+    }
 
 }
--- project/src/main/java/by/htp/ex/service/INewsService.java
+++ project/src/main/java/by/htp/ex/service/INewsService.java
@@ -1,24 +1,24 @@
 package by.htp.ex.service;
 
-import java.sql.SQLException;
+
 import java.util.List;
 
 import by.htp.ex.bean.Category;
 import by.htp.ex.bean.News;
-import by.htp.ex.dao.DaoException;
-import by.htp.ex.dao.connectionPool.ConnectionPoolException;
+
+
 
 public interface INewsService {
-    void save(News news);
-    void find(int idNews);
-    void update(News news) throws SQLException, ConnectionPoolException;
-    void delete(String idNews) throws SQLException;
-    void addLike(String newsId) throws SQLException;	
+    void save(News news) throws ServiceException;
+    void find(int idNews) throws ServiceException;
+    void update(News news) throws ServiceException;
+    void delete(String idNews) throws ServiceException;
+    void addLike(String newsId) throws ServiceException;	
     
-    List<News> sortByCategory(String category) throws ServiceException, ConnectionPoolException, SQLException, DaoException;
-    List<Category> findAllCategoryes() throws ConnectionPoolException, SQLException, DaoException;
-    List<News> latestList(int count)  throws ServiceException, ConnectionPoolException, SQLException, DaoException;
-    List<News> list()  throws ServiceException, ConnectionPoolException, SQLException, DaoException;
-    News findById(String id) throws ServiceException, ConnectionPoolException, SQLException, DaoException;
+    List<News> sortByCategory(String category) throws ServiceException;
+    List<Category> findAllCategoryes() throws ServiceException;
+    List<News> latestList(int count)  throws ServiceException;
+    List<News> list()  throws ServiceException;
+    News findById(String id) throws ServiceException;
 	
 }
